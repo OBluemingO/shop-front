b8efda263fd93c5a654c17a140852f1f7b54ccf0
OBluemingO <phayuphat118@gmail.com>
2022-12-02 00:35:35 +0700

    asdasd

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -4,16 +4,16 @@ const customInstance = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
   headers: {
     "Access-Control-Allow-Origin": '*',
-    'Access-Control-Allow-Method': 'GET',
-  }
+    // 'Access-Control-Allow-Method': 'GET',
+  },
 })
-customInstance.interceptors.request.use((config) => {
-  if (typeof window !== 'undefined') {
-    const token = localStorage.getItem('access_token')
-    config.headers.Authorization = token ? `Bearer ${token}` : ''
-    return config
-  }
-  return config
-})
+// customInstance.interceptors.request.use((config) => {
+//   if (typeof window !== 'undefined') {
+//     const token = localStorage.getItem('access_token')
+//     config.headers.Authorization = token ? `Bearer ${token}` : ''
+//     return config
+//   }
+//   return config
+// })
 
 export default customInstance
